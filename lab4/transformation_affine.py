83a2c788e826fe635a328d9c4ab2b7ef4e25b381
Jean-François Bourdon <32580398+jfbourdon@users.noreply.github.com>
2020-02-16 16:31:54 -0500

    Fonction construction matrice A et print

--- lab4/transformation_affine.py
+++ lab4/transformation_affine.py
@@ -25,40 +25,49 @@ file_xy1_points = "G03_points_xy1.txt"
 file_xy2_points = "G03_points_xy2.txt"
 file_xy1_valid = "G03_validation_xy1.txt"
 file_xy2_valid = "G03_validation_xy2.txt"
-header_length = 5
 
-# Fonction de chargement des fichiers
+# Fonction de chargement des fichiers de coordonnées
 def load_xy(filepath, header_length):
     with open(filepath, "r") as f:
         lines = f.readlines()
     
-    nb_coord = len(lines) - header_length
+    lines_valid = [line for line in lines if line[0] != "%"]
+    nb_coord = len(lines_valid)
     arr_xy = np.zeros((nb_coord, 2), dtype=np.float32)
+    
     for ii in range(0, nb_coord):
-        ls_number = lines[ii + header_length].split("\t")
+        ls_number = lines_valid[ii].split("\t")
         number_float = [float(number.strip()) for number in ls_number]
         arr_xy[ii,:] = (number_float[0], number_float[1])
     
     return arr_xy
 
-# Chargement des fichiers de points
-arr_xy1_points = load_xy(os.path.join(wdir, file_xy1_points), header_length)
-arr_xy2_points = load_xy(os.path.join(wdir, file_xy2_points), header_length)
+#Fonction de construction de la matrice A
+def make_A(arr_xy):
+    nb_coord = arr_xy.shape[0]
+    arr_A = np.zeros((nb_coord*2,6), dtype=np.float32)
+    ii = 0
+    for line in range(0, nb_coord*2, 2):
+        arr_A[line,0] = arr_xy[ii,0]
+        arr_A[line,1] = arr_xy[ii,1]
+        arr_A[line,2] = 1
+        arr_A[line+1,3] = arr_xy[ii,0]
+        arr_A[line+1,4] = arr_xy[ii,1]
+        arr_A[line+1,5] = 1
+        ii += 1
+    
+    return arr_A
+
+# ESTIMATION DES PARAMÈTRES
+# Chargement des fichiers de coordonnées
+arr_xy1_points = load_xy(os.path.join(wdir, file_xy1_points))
+arr_xy2_points = load_xy(os.path.join(wdir, file_xy2_points))
 
 # Construction de la matrice A
-nb_coord = arr_xy1_points.shape[0]
-arr_A = np.zeros((nb_coord*2,6), dtype=np.float32)
-ii = 0
-for line in range(0, nb_coord*2, 2):
-    arr_A[line,0] = arr_xy1_points[ii,0]
-    arr_A[line,1] = arr_xy1_points[ii,1]
-    arr_A[line,2] = 1
-    arr_A[line+1,3] = arr_xy1_points[ii,0]
-    arr_A[line+1,4] = arr_xy1_points[ii,1]
-    arr_A[line+1,5] = 1
-    ii += 1
+arr_A = make_A(arr_xy1_points)
 
 # Construction de la matrice L
+nb_coord = arr_xy2_points.shape[0]
 arr_L = arr_xy2_points.reshape((nb_coord*2,1))
 
 # Produit matriciel pour trouver les 6 coefficients (a,b,c,d,e,f)
@@ -82,33 +91,30 @@ print("\n".join(["Theta: " + str(round(math.degrees(theta),4)) + "°",
                  "Tx:    " + str(round(c,2)) + " cm",
                  "Ty:    " + str(round(f,2)) + " cm",
                  "Sx:    " + str(round(math.degrees(Sx),4)) + "°",
-                 "Sy:    " + str(round(math.degrees(Sy),4)) + "°"]))
+                 "Sy:    " + str(round(math.degrees(Sy),4)) + "°",
+                 "",
+                 "a: " + str(round(a,2)),
+                 "b: " + str(round(b,2)),
+                 "c: " + str(round(c,2)),
+                 "d: " + str(round(d,2)),
+                 "e: " + str(round(e,2)),
+                 "f: " + str(round(f,2))]))
 
 
 
 # VALIDATION
-# Chargement des fichiers de points
-arr_xy1_valid = load_xy(os.path.join(wdir, file_xy1_valid), header_length)
-arr_xy2_valid = load_xy(os.path.join(wdir, file_xy2_valid), header_length)
-nb_coord = arr_xy1_valid.shape[0]
+# Chargement des fichiers de coordonnées
+arr_xy1_valid = load_xy(os.path.join(wdir, file_xy1_valid))
+arr_xy2_valid = load_xy(os.path.join(wdir, file_xy2_valid))
 
 # Construction de la matrice A
-nb_coord = arr_xy1_valid.shape[0]
-arr_A = np.zeros((nb_coord*2,6), dtype=np.float32)
-ii = 0
-for line in range(0, nb_coord*2, 2):
-    arr_A[line,0] = arr_xy1_valid[ii,0]
-    arr_A[line,1] = arr_xy1_valid[ii,1]
-    arr_A[line,2] = 1
-    arr_A[line+1,3] = arr_xy1_valid[ii,0]
-    arr_A[line+1,4] = arr_xy1_valid[ii,1]
-    arr_A[line+1,5] = 1
-    ii += 1
+arr_A = make_A(arr_xy1_valid)
 
 # Calcul de la matrice L transformée et arroundie au centième de centimètre
 arr_L = np.around(arr_A @ arr_X, 2)
 
 # Comparaison avec les coordonnées validées transformées
+nb_coord = arr_xy1_valid.shape[0]
 if np.all(arr_L.reshape(nb_coord,2) == arr_xy2_valid):
     print("Transformation affine réussie!")
 else:
